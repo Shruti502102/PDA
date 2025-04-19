8bb4bb2f1506f136f99a944f164e9a95d2778d24
Shruti Srivastava <146112301+Shruti502102@users.noreply.github.com>
2025-04-19 17:53:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+db.collection.insertOne({
+  "name":"Shruti",
+  "email":"shrity56@gmail.com",
+  "age":21,
+  
+});
+db.collection.insertMany([
+  {
+    "name":"yrt","age":21,"email":"sharat7763@gmail.com"
+  },
+  {
+    "name":"guihgmnt","age":24,"email":"mahi433@gmail.com"
+  }
+  ]
+  
+);
+db.collection.find();
